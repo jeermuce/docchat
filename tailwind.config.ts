7134497d6b33ae2216c964b37b008c441299aa59
jeermuce <ernesto_mc.dev@proton.me>
2024-10-18 14:10:30 -0600

    Add css changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 import type { PluginAPI } from "tailwindcss/types/config";
 
 const config: Config = {
-    darkMode: ["class"],
+    lightMode: ["class"],
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
