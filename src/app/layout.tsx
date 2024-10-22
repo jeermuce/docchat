bec0d381f5a8ddf3c058355d18aeb5081fec8214
jeermuce <ernesto_mc.dev@proton.me>
2024-10-22 04:35:21 -0600

    add fallback (will likely never be used, but still)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
     title: "DocChat",
@@ -29,7 +30,22 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                {children}
+                {children ? (
+                    children
+                ) : (
+                    <a
+                        href="https://github.com/jeermuce/docchat/issues"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <Button
+                            variant={"destructive"}
+                            className="absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2"
+                        >
+                            DocChat is dead, please click here to rise an issue
+                        </Button>
+                    </a>
+                )}
             </body>
         </html>
     );
