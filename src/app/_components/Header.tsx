fb0ebff0ebbf979b8c12e285e2cef93748b9e8f0
jeermuce <ernesto_mc.dev@proton.me>
2024-10-22 03:21:44 -0600

    Fix center

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -5,9 +5,11 @@ import Nav from "./Nav";
 function Header(props: React.HTMLProps<HTMLDivElement>) {
     return (
         <header className={`${props.className} content-grid`}>
-            <div className="big flex flex-row flex-nowrap w-full justify-between">
-                <Nav className="m-0">
-                    <Link href="/home">ome</Link>
+            <div className="big flex items-center flex-row flex-nowrap w-full justify-between">
+                <Nav className="m-0 ">
+                    <Link href="/home">
+                        <h2 className="">Home</h2>
+                    </Link>
                 </Nav>
 
                 <Nav>
