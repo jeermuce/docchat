1bf5c417fc0aa9c6ebff919dc55763adf9c1fa6d
jeermuce <ernesto_mc.dev@proton.me>
2024-10-22 04:30:28 -0600

    add a button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 import Footer from "./_components/Footer";
 import Header from "./_components/Header";
@@ -6,7 +7,14 @@ export default function Home() {
     return (
         <>
             <Header className="bg-background" />
-            <Main className="bg-transparent" />
+            <Main className="bg-transparent">
+                <Button
+                    variant={"outline"}
+                    className="absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2"
+                >
+                    DocChat is live
+                </Button>
+            </Main>
             <Footer className="bg-background" />
         </>
     );
