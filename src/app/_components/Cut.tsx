73aa4e3b0d75bf3e5b6318af61755f6bef75fdc4
jeermuce <ernesto_mc.dev@proton.me>
2024-10-22 18:17:21 -0600

    Make spacer more usable

--- src/app/_components/Cut.tsx
+++ src/app/_components/Cut.tsx
@@ -5,13 +5,21 @@ function Cut({
     children,
     className,
     zIndex = 1,
+    width = "all",
     ...props
-}: { children?: React.ReactNode; className?: string; zIndex?: number }) {
+}: {
+    children?: React.ReactNode;
+    className?: string;
+    zIndex?: number;
+    width?: "all" | "big" | "mid";
+}) {
     return (
         <div
+            {...props}
             className={cn(
                 className,
-                "h-8  w-[100%] all max-w-[100%] min-w-[100%] rounded-ellipse -my-2  rounded-t-none",
+                "h-8  w-[100%]  max-w-[100%] min-w-[100%] rounded-ellipse -my-2  rounded-t-none",
+                width,
             )}
             style={{ zIndex: zIndex }}
         />
